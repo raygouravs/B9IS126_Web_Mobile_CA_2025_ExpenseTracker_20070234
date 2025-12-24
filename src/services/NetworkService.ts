bf34aeb45cb5a742b55fd70cccbc043483c5d587
gouravray <gourav.sankar93@gmail.com>
2025-12-24 23:47:33 +0000

    Network service added

---
+++ src/services/NetworkService.ts
@@ -0,0 +1,12 @@
+/*
+    REFERENCE: Ionic (2025) Network Capacitor Plugin API. Available at: https://ionicframework.com/docs/native/network
+*/
+
+import { Network } from '@capacitor/network';
+
+export class NetworkConnectivity {
+    static async isNetworkAvailable(): Promise<boolean> {
+        const status = await Network.getStatus();
+        return status.connected ?? false;
+    }
+}

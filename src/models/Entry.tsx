3a6efce76f9727f507651f0d72434c98e52998f8
gouravray <gourav.sankar93@gmail.com>
2025-12-24 19:28:17 +0000

    added Entry model class

---
+++ src/models/Entry.tsx
@@ -0,0 +1,13 @@
+
+export type EntryType = 'income' | 'expense';
+
+export interface Entry {
+  id: string;
+  type: EntryType;
+  date: string; // YYYY-MM-DD
+  amount: number;
+  expense_category?: string;
+  income_source?: string;
+}
+
+
